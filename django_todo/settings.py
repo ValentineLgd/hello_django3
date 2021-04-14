0a70ea738af7544d939f9df5b07581639deba8b6
ValentineLgd <valentinelagand@gmail.com>
2021-04-14 15:10:08 +0000

    lala

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-977!+d3t(*wkkf&_rz9_x8lnz2pdcu=@#7_x@=!+(m8z*!p4x-'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-977!+d3t(*wkkf&_rz9_x8lnz2pdcu=@#7_x@=!+(m8z*!p4x-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,9 +82,11 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #     }
 # }
 
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
