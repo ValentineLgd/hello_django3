50f3c056aa08274c02d11d92ccf4f38af48cccee
ValentineLgd <valentinelagand@gmail.com>
2021-04-14 15:12:06 +0000

    lala

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-977!+d3t(*wkkf&_rz9_x8lnz2pdcu=@#7_x@=!+(m8z*!p4x-')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-977!+d3t(*wkkf&_rz9_x8lnz2pdcu=@#7_x@=!+(m8z*!p4x-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
